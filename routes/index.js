f7491abbfa32a2da4f9e09de67ae9596e1f77a81
Mario Tacke <tacke.mario@gmail.com>
2018-05-05 15:53:26 -0700

    feat: add POST route to accept incoming data

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,25 @@
 const Router = require('koa-router');
-const util = require('util');
 
 const router = new Router();
 
 router.get('/', async (ctx, next) => {
+  // TODO: validate verification token
   const verificationToken = ctx.request.header['verification-token'];
   const challenge = ctx.request.query['challenge'];
 
-  util.log('token', verificationToken, 'challenge', challenge);
-
   ctx.body = challenge;
 });
+
+router.post('/', async (ctx, next) => {
+  const { Meta, Patient } = ctx.request.body;
+
+  if (Meta.DataModel !== 'PatientAdmin' || Meta.EventType !== 'NewPatient') {
+    ctx.throw(400);
+  }
+
+  // TODO: trigger processing
+
+  ctx.body = {};
+})
 
 module.exports = router;
