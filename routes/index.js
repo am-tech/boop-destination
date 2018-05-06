249cad5155dfcd62a31d3a0e4d1839ba4b48b8ac
Mario Tacke <tacke.mario@gmail.com>
2018-05-06 01:09:22 -0700

    feat: add additional carrier

--- routes/index.js
+++ routes/index.js
@@ -11,14 +11,21 @@ router.get('/', async (ctx, next) => {
   ctx.body = challenge;
 });
 
+const templateMapping = {
+  'f91b0c80-8038-404c-996a-c19d81fc8133': 'precision-dental.pdf',
+  'f91b0c80-8038-404c-996a-c19d81fc8134': 'absolute-dental.pdf',
+};
+
 router.post('/', async (ctx, next) => {
   const { Meta, Patient } = ctx.request.body;
+
+  const id = 'f91b0c80-8038-404c-996a-c19d81fc8134';
 
   if (Meta.DataModel !== 'PatientAdmin' || Meta.EventType !== 'NewPatient') {
     ctx.throw(400);
   }
 
-  await fillPdf('Patient_Info_with_fields.pdf', Patient);
+  await fillPdf(templateMapping[id], Patient);
 
   ctx.body = {};
 })
